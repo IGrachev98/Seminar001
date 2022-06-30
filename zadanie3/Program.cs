6265ba2047f1b28b17a6e0a7a020b5021570f459
Ivan <malcevivan98@mail.ru>
2022-06-30 22:42:09 +0300

    Является ли число чётным

--- zadanie3/Program.cs
+++ zadanie3/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("Введите ваше число");
+double a = Convert.ToDouble(Console.ReadLine());
+if (a%2==0) {
+    Console.WriteLine("Верно");
+}
+else {
+    Console.WriteLine("Введите другое число");
+}
