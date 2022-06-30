b2ff3dc1d56c1154bc8f65cbf55973faa8c77837
Ivan <malcevivan98@mail.ru>
2022-06-30 21:54:55 +0300

    Сделали - Нахождение максималоного из трёх чисел

--- Zadanie2/Program.cs
+++ Zadanie2/Program.cs
@@ -1,2 +1,23 @@
-﻿
-
+﻿Console.WriteLine("Введите первое число");
+int max;
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b =  Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число ");
+int c =  Convert.ToInt32(Console.ReadLine());
+if(a>b){
+    max=a;
+}
+if(a>c){
+    max=a;
+}
+else {
+    max=c;
+}
+if(b>a){
+    max=b;
+}
+if(c>b){
+    max=c;
+}
+Console.WriteLine("Максимальное число:"+ max );
